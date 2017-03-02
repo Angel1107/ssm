f2641cd9583522d5fd5f2d3e28d735b1275b768b
liuchuang <1195805998@qq.com>
2017-03-02 22:12:27 +0800

    测试 Idea 提交分支

---
+++ src/test/java/ssmm/IdeaTest.java
@@ -0,0 +1,10 @@
+package ssmm;
+
+/**
+ * Created by Lc on 2017/3/2.
+ */
+public class IdeaTest {
+    public static void main(String[] args){
+        System.out.println("我的 Idea Push");
+    }
+}
